cb3e569742d5bdf472340da8776400fd75cab172
GreatHayat <greathayat@email.com>
2022-07-15 18:25:10 +0500

    Updates

---
+++ precache-manifest.bdcb38b989ac098ce0cf0551e118c266.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f63ebfbdfd5ac158ce6fc8dbe1dc21",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "ce5219a2f1f05d75a0f1",
+    "url": "/personal-website/static/css/main.df7d4acd.chunk.css"
+  },
+  {
+    "revision": "1ead01054e37f5e59959",
+    "url": "/personal-website/static/js/2.6c9a8fbf.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/personal-website/static/js/2.6c9a8fbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5219a2f1f05d75a0f1",
+    "url": "/personal-website/static/js/main.b9745174.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/personal-website/static/js/main.b9745174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e173746013bd335247f",
+    "url": "/personal-website/static/js/runtime-main.3ccbf239.js"
+  },
+  {
+    "revision": "1168f54731a7cee464e15bb290359c13",
+    "url": "/personal-website/static/media/greathayat.1168f547.png"
+  }
+]);
